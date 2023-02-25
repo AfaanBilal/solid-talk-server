a803292965bfbf1c482afa0ddb5a5660814efcca
Afaan Bilal <afaanbilal@gmail.com>
2023-02-25 10:23:06 +0530

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#solid-talk-server
+==================
+
+Author: **[Afaan Bilal](https://afaan.dev)**
+
+Link: **[#solid-talk](https://afaan.dev/solid-talk)**
+
+## Introduction
+**#solid-talk-server** is the socket.io server for [#solid-talk](https://github.com/AfaanBilal/solid-talk).
+
+## Contributing
+All contributions are welcome. Please create an issue first for any feature request
+or bug. Then fork the repository, create a branch and make any changes to fix the bug
+or add the feature and create a pull request. That's it!
+Thanks!
+
+## License
+**#solid-talk-server** is released under the MIT License.
+Check out the full license [here](LICENSE).
---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * #solid-talk
+ *
+ * @author Afaan Bilal https://afaan.dev
+ * @link   https://afaan.dev/solid-talk
+ */
+
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+app.get("/", (req, res) => res.send("#solid-talk @ afaan.dev/solid-talk"));
+
+let users = [];
+
+io.on("connection", socket => {
+    const user = { id: socket.id, name: "", avatar: "" };
+    users.push(user);
+
+    io.emit("users", users);
+
+    socket.on("disconnect", () => {
+        users = users.filter(o => o.id !== user.id);
+        io.emit("users", users);
+    });
+
+    socket.on("message", m => io.emit("message", m));
+    socket.on("user-updated", m => {
+        users[users.findIndex(o => o.id === m.id)] = m;
+        io.emit("users", users);
+    });
+});
+
+server.listen(80, () => console.log("Server started on :80"));
